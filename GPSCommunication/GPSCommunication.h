0d9d65cb8ab428e5b73a0a6148d1b6e9d7319351
Erik Larsen <erik_larsen@icloud.com>
2013-11-09 13:27:03 -0800

    Initial Commit

---
+++ GPSCommunication/GPSCommunication.h
@@ -0,0 +1,13 @@
+//
+//  GPSCommunication.h
+//  GPSCommunication
+//
+//  Created by Erik Larsen on 11/9/13.
+//  Copyright (c) 2013 Erik Larsen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GPSCommunication : NSObject
+
+@end
---
+++ GPSCommunication/GPSCommunication.m
@@ -0,0 +1,13 @@
+//
+//  GPSCommunication.m
+//  GPSCommunication
+//
+//  Created by Erik Larsen on 11/9/13.
+//  Copyright (c) 2013 Erik Larsen. All rights reserved.
+//
+
+#import "GPSCommunication.h"
+
+@implementation GPSCommunication
+
+@end
